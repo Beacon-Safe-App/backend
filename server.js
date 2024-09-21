8e1d0fdfd26583e840ef62e5bc171713e7550f7d
Konstantin <konstantin.kaminskiy@outlook.com>
2024-09-21 15:30:50 -0400

    adds CORS to enable login

--- server.js
+++ server.js
@@ -4,11 +4,30 @@ const express = require("express");
 const app = express();
 const routes = require('./routes/index.js');
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 app.disable("x-powered-by")
 app.use(cookieParser())
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+
+const allowedOrigins = [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    // Add other origins as needed
+]
+
+const corsOptionsDelegate = (req, callback) => {
+    let corsOptions
+    if (allowedOrigins.includes(req.header('Origin'))) {
+      corsOptions = { origin: true, credentials: true }
+    } else {
+      corsOptions = { origin: false }
+    }
+    callback(null, corsOptions)
+}
+
+app.use(cors(corsOptionsDelegate))
 
 app.use('/', routes)
 app.use((req, res) => {res.status(404).json({message: "NOT A PROPER ROUTE", data: [JSON.stringify(req.path)]})})
